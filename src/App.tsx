22b850d663032f116d76a16bba5d2cd455da455b
yousunzoo <dpfvmffjqm@gmail.com>
2025-02-27 21:10:03 +0900

    chore: tailwindcss 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return <h1>Self-Paced React</h1>;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EC4A0A',
+        sub: '#F6A88A',
+        black: {
+          100: '#D0D5DD',
+          200: '#667085',
+          300: '#344054',
+          default: '#000000',
+        },
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require('tailwindcss-animate')],
+};
