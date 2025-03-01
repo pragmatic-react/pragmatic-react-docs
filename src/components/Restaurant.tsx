a7d0bfe95e0c2741b2ce8e432c194947fcc91248
pithesun <wonjh0730@naver.com>
2025-03-02 01:58:43 +0900

    [기능] 레스토랑 데이터 페칭 및 UI 렌더링

--- src/components/Restaurant.tsx
+++ src/components/Restaurant.tsx
@@ -1,5 +1,47 @@
-const RestaurantSection = () => {
-  return <h1>Restaurant</h1>;
+import useRestaurants from "../hooks/useRestaurants";
+import { Restaurant as RestaurantType } from "../models";
+
+import Section from "./Section";
+
+const Restaurant = ({ restaurant }: { restaurant: RestaurantType }) => {
+  return (
+    <li className="restaurant" key={restaurant.id}>
+      <div className="restaurant__category">
+        <img
+          src={restaurant.getCategeryImgSrc()}
+          alt={restaurant.category}
+          className="category-icon"
+        />
+      </div>
+      <div className="restaurant__info">
+        <h3 className="restaurant__name text-subtitle">{restaurant.name}</h3>
+        <p className="restaurant__description text-body">
+          {restaurant.description}
+        </p>
+      </div>
+    </li>
+  );
 };
 
-export { RestaurantSection };
+const RestaurantList = ({ data }: { data: RestaurantType[] | undefined }) => {
+  return (
+    <ul className="restaurant-list">
+      {data &&
+        data.map((restaurant) => (
+          <Restaurant restaurant={restaurant} key={restaurant.id} />
+        ))}
+    </ul>
+  );
+};
+
+const RestaurantSection = () => {
+  const { restaurants } = useRestaurants();
+
+  return (
+    <Section className="restaurant-list-container">
+      <RestaurantList data={restaurants} />
+    </Section>
+  );
+};
+
+export { Restaurant, RestaurantList, RestaurantSection };
