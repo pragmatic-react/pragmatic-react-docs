4e2e6d35bebe0d7d4c87d28bcb41e319f5933d64
mmyeon <mindfulyeon@gmail.com>
2025-03-01 23:31:04 +0900

    [feat] 레스토랑 목록  카테고리 필터 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
+import CategoryFilter from "./components/CategoryFilter";
+import Header from "./components/Header";
+import RestaurantDetailModal from "./components/RestaurantDetailModal";
+import RestaurantList from "./components/RestaurantList";
+import { restaurants } from "./restaurantData";
+
 function App() {
-  return <h1>Self-Paced React</h1>;
+  const [category, setCategory] = useState("전체");
+  const filteredRestaurants =
+    category === "전체"
+      ? restaurants
+      : restaurants.filter((restaurant) => restaurant.category === category);
+
+  return (
+    <>
+      <Header />
+
+      <main>
+        <CategoryFilter category={category} onChangeCategory={setCategory} />
+        <RestaurantList restaurants={filteredRestaurants} />
+      </main>
+
+      <aside>
+        <RestaurantDetailModal />
+
+        {/* <AddRestaurantModal /> */}
+      </aside>
+    </>
+  );
 }
 
 export default App;
