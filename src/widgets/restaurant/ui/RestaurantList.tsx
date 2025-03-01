74546f22b1eedc7f19fb97e733619ce0dae4f48a
yousunzoo <dpfvmffjqm@gmail.com>
2025-03-01 18:34:10 +0900

    feat: 식당 카드 클릭 시 상세 정보 drawer 출력하도록 처리

--- src/widgets/restaurant/ui/RestaurantList.tsx
+++ src/widgets/restaurant/ui/RestaurantList.tsx
@@ -1,15 +1,34 @@
+import { useState } from 'react';
+
+import { useModal } from '@features/modal';
+import { RestaurantDetailDrawer } from '@features/restaurant';
+
 import { Restaurant, RestaurantCard } from '@entities/restaurant';
 
 import data from '../../../../db.json';
 
 export const RestaurantList = () => {
   const { restaurants } = data as { restaurants: Restaurant[] };
+  const [selected, setSelected] = useState<Restaurant | null>(null);
+  const { openModal } = useModal();
+
+  const handleCardClick = (id: string) => () => {
+    const restaurant = restaurants.find((restaurant) => restaurant.id === id);
+    if (restaurant) {
+      setSelected(restaurant);
+      openModal();
+    }
+  };
 
   return (
-    <ul className="restaurant-list-container">
-      {restaurants.map((restaurant) => (
-        <RestaurantCard restaurant={restaurant} onClick={() => {}} />
-      ))}
-    </ul>
+    <>
+      <ul className="restaurant-list-container">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} onClick={handleCardClick} />
+        ))}
+      </ul>
+
+      <RestaurantDetailDrawer selected={selected} />
+    </>
   );
 };
