457aa316c7bbdc00275121f3724676bb3e331bfb
hanna244 <shhn0509@gmail.com>
2025-03-09 03:56:15 +0900

    ✨ 음식점 추가 데이터 연동

--- src/components/createRestaurant.tsx
+++ src/components/createRestaurant.tsx
@@ -1,19 +1,89 @@
+import { useRef } from "react";
+import RestaurantService from "../services/restaurantApi";
+import { RestaurantCategoryType } from "../services/restaurantIType";
+
 export default function CreateRestaurant({
   closeModal,
+  refetch,
 }: {
   closeModal: () => void;
+  refetch: () => Promise<void>;
 }) {
+  const name = useRef("");
+  const category = useRef("");
+  const description = useRef("");
+
+  async function onSubmit() {
+    const data = {
+      name: name.current,
+      category: category.current,
+      description: description.current,
+    };
+
+    try {
+      await RestaurantService.CreateInfo({
+        data: {
+          id: `a${Math.floor(Math.random() * 90) + 10}`,
+          name: data.name as string,
+          category: data.category as RestaurantCategoryType,
+          description: data.description as string,
+        },
+      });
+
+      await refetch();
+      closeModal();
+    } catch (e) {
+      window.alert("예기치못한 에러가 발생했습니다.");
+      console.error(e);
+    }
+  }
+
+  //   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //     e.preventDefault();
+
+  //     const formData = new FormData(e.currentTarget);
+
+  //     const data = {
+  //       name: formData.get("name"),
+  //       category: formData.get("category"),
+  //       description: formData.get("description"),
+  //     };
+
+  //     try {
+  //       await RestaurantService.CreateInfo({
+  //         data: {
+  //           id: `a${Math.floor(Math.random() * 90) + 10}`,
+  //           name: data.name as string,
+  //           category: data.category as RestaurantCategoryType,
+  //           description: data.description as string,
+  //         },
+  //       });
+
+  //       await refetch();
+  //       closeModal();
+  //     } catch (e) {
+  //       window.alert("예기치못한 에러가 발생했습니다.");
+  //       console.error(e);
+  //     }
+  //   };
+
   return (
     <form>
       <div className="form-item form-item--required">
         <label htmlFor="category" className="text-caption">
           카테고리
         </label>
-        <select name="category" id="category" required>
+        <select
+          name="category"
+          id="category"
+          required
+          onChange={(e) => {
+            category.current = e.target.value;
+          }}
+        >
           <option value="">선택해 주세요</option>
           <option value="한식">한식</option>
           <option value="중식">중식</option>
-          <option value="일식">일식</option>
           <option value="양식">양식</option>
           <option value="아시안">아시안</option>
           <option value="기타">기타</option>
@@ -24,7 +94,15 @@ export default function CreateRestaurant({
         <label htmlFor="name" className="text-caption">
           이름
         </label>
-        <input type="text" name="name" id="name" required />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          onBlur={(e) => {
+            name.current = e.target.value;
+          }}
+        />
       </div>
 
       <div className="form-item">
@@ -36,6 +114,9 @@ export default function CreateRestaurant({
           id="description"
           cols={30}
           rows={5}
+          onBlur={(e) => {
+            description.current = e.target.value;
+          }}
         ></textarea>
         <span className="help-text text-caption">
           메뉴 등 추가 정보를 입력해 주세요.
@@ -46,11 +127,17 @@ export default function CreateRestaurant({
         <button
           type="button"
           className="button button--secondary text-caption"
-          onClick={closeModal}
+          onClick={() => {
+            closeModal();
+          }}
         >
           취소하기
         </button>
-        <button className="button button--primary text-caption">
+        <button
+          type="button"
+          className="button button--primary text-caption"
+          onClick={onSubmit}
+        >
           추가하기
         </button>
       </div>
