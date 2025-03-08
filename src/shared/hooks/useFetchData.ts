6a28459f2bbf5f1b9c534b43ade56bce9568cdd1
yousunzoo <dpfvmffjqm@gmail.com>
2025-03-08 18:56:46 +0900

    refactor: useFetchData 훅 refetch 추가 및 최적화

--- src/shared/hooks/useFetchData.ts
+++ src/shared/hooks/useFetchData.ts
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const useFetchData = <Data, Params = void>({
   fetchFunction,
@@ -15,27 +14,27 @@ export const useFetchData = <Data, Params = void>({
   const [isSuccess, setIsSuccess] = useState(false);
   const [isError, setIsError] = useState(false);
 
+  const fetchData = useCallback(async () => {
+    setIsPending(true);
+    setIsError(false);
+    setIsSuccess(false);
+
+    try {
+      const result = await fetchFunction();
+      setData(result);
+      setIsSuccess(true);
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsPending(false);
+    }
+  }, [fetchFunction]);
+
   useEffect(
     function handleFetchDataEffect() {
       if (!enabled) return;
 
       let intervalId: NodeJS.Timeout | null = null;
-
-      const fetchData = async () => {
-        setIsPending(true);
-        setIsError(false);
-        setIsSuccess(false);
-
-        try {
-          const result = await fetchFunction();
-          setData(result);
-          setIsSuccess(true);
-        } catch (error) {
-          setIsError(true);
-        } finally {
-          setIsPending(false);
-        }
-      };
 
       fetchData();
 
@@ -47,8 +46,8 @@ export const useFetchData = <Data, Params = void>({
         if (intervalId) clearInterval(intervalId);
       };
     },
-    [enabled, refetchInterval],
+    [enabled, refetchInterval, fetchData],
   );
 
-  return { data, isPending, isSuccess, isError };
+  return { data, isPending, isSuccess, isError, refetch: fetchData };
 };
