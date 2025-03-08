39b9cefbb683ebee7bc3a6d82f944998db6297f7
yousunzoo <dpfvmffjqm@gmail.com>
2025-03-08 20:26:05 +0900

    feat: 레스토랑 스켈레톤 컴포넌트 추가

---
+++ src/shared/ui/skeleton/RestaurantSkeleton.tsx
@@ -0,0 +1,10 @@
+export const RestaurantSkeleton = () => {
+  return (
+    <div className="flex w-full flex-col gap-2 px-5 pt-5">
+      <div className="h-[110px] w-full animate-pulse rounded-lg bg-gray-100" />
+      <div className="h-[110px] w-full animate-pulse rounded-lg bg-gray-100" />
+      <div className="h-[110px] w-full animate-pulse rounded-lg bg-gray-100" />
+      <div className="h-[110px] w-full animate-pulse rounded-lg bg-gray-100" />
+    </div>
+  );
+};
