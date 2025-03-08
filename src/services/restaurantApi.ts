044e46cdfea791b6a187710b1b210c1ae8ee8a57
hanna244 <shhn0509@gmail.com>
2025-03-09 03:53:34 +0900

    ✨ restaurant service 코드 추가

---
+++ src/services/restaurantApi.ts
@@ -0,0 +1,25 @@
+import { AxiosRequestConfig } from "axios";
+import { axios } from ".";
+import { RestaurantItemType } from "./restaurantIType";
+
+export default class RestaurantService {
+  static ReadInfoList({ config }: { config?: AxiosRequestConfig<any> }) {
+    return axios.get("/restaurants", config);
+  }
+
+  static CreateInfo({
+    data,
+    config,
+  }: {
+    data: RestaurantItemType & { id: string };
+    config?: AxiosRequestConfig<any>;
+  }) {
+    return axios.post("/restaurants", JSON.stringify(data), {
+      ...config,
+      headers: {
+        "Content-Type": "application/json",
+        ...config?.headers,
+      },
+    });
+  }
+}
