b6eb4adb7b1c887a8a56166052a818154af207e8
hanna244 <shhn0509@gmail.com>
2025-03-09 03:55:38 +0900

    ✨ 음식점 목록 데이터 연동

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import { useState, useReducer } from "react";
+import { useState, useReducer, useEffect } from "react";
 import RestaurantItem from "./components/restaurantItem";
 import RestaurantModal from "./components/restaurantModal";
-import { restaurants } from "../db.json";
-import { Category } from "./components/restaurantItem";
+import { CategoryType } from "./components/restaurantItem";
+import RestaurantService from "./services/restaurantApi";
+import CreateRestaurant from "./components/createRestaurant";
 
 interface ModalState {
   detail: boolean;
@@ -18,7 +19,7 @@ interface Restaurant {
   id: string;
   name: string;
   description: string;
-  category: Category;
+  category: CategoryType;
 }
 
 export default function Container() {
@@ -48,13 +49,25 @@ export default function Container() {
     id: "",
     name: "",
     description: "",
-    category: { id: "", name: "" },
+    category: "한식",
   });
 
   function handleClickRestaurantItem(restaurantItem: Restaurant) {
     toggleModal("detail", true)();
     setSelectedRestaurant(restaurantItem);
   }
+
+  /* ------------------------------- Data fetch ------------------------------- */
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+
+  const fetchRestaurants = async () => {
+    const response = await RestaurantService.ReadInfoList({});
+    setRestaurants(response.data);
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
 
   return (
     <div>
@@ -125,57 +138,10 @@ export default function Container() {
           isOpen={isOpenModal.create}
           onClose={toggleModal("create", false)}
         >
-          <form>
-            <div className="form-item form-item--required">
-              <label htmlFor="category" className="text-caption">
-                카테고리
-              </label>
-              <select name="category" id="category" required>
-                <option value="">선택해 주세요</option>
-                <option value="한식">한식</option>
-                <option value="중식">중식</option>
-                <option value="일식">일식</option>
-                <option value="양식">양식</option>
-                <option value="아시안">아시안</option>
-                <option value="기타">기타</option>
-              </select>
-            </div>
-
-            <div className="form-item form-item--required">
-              <label htmlFor="name" className="text-caption">
-                이름
-              </label>
-              <input type="text" name="name" id="name" required />
-            </div>
-
-            <div className="form-item">
-              <label htmlFor="description" className="text-caption">
-                설명
-              </label>
-              <textarea
-                name="description"
-                id="description"
-                cols="30"
-                rows="5"
-              ></textarea>
-              <span className="help-text text-caption">
-                메뉴 등 추가 정보를 입력해 주세요.
-              </span>
-            </div>
-
-            <div className="button-container">
-              <button
-                type="button"
-                className="button button--secondary text-caption"
-                onClick={toggleModal("create", false)}
-              >
-                취소하기
-              </button>
-              <button className="button button--primary text-caption">
-                추가하기
-              </button>
-            </div>
-          </form>
+          <CreateRestaurant
+            closeModal={toggleModal("create", false)}
+            refetch={fetchRestaurants}
+          />
         </RestaurantModal>
       </aside>
     </div>
