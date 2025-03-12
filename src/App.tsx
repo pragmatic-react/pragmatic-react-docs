ca19dbdcff9e7b132725a92c57dd452b7ac72daa
mmyeon <mindfulyeon@gmail.com>
2025-03-12 12:59:10 +0900

    [refactor] RestaurantProvider 감싸는 위치 App 레벨로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import Header from "./components/Header";
 import CategoryFilter from "./components/CategoryFilter";
 import { CategorySelectList } from "./types/restaurant";
 import RestaurantList from "./components/RestaurantList";
+import { RestaurantProvider } from "./RestaurantProvider";
 
 function App() {
   const [selectedCategory, setSelectedCategory] =
     useState<CategorySelectList>("전체");
 
   return (
-    <>
+    <RestaurantProvider>
       <Header />
 
       <main>
@@ -22,7 +23,7 @@ function App() {
 
         <RestaurantList selectedCategory={selectedCategory} />
       </main>
-    </>
+    </RestaurantProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
-import { RestaurantProvider } from "./RestaurantProvider.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RestaurantProvider>
-      <App />
-    </RestaurantProvider>
+    <App />
   </React.StrictMode>
 );
