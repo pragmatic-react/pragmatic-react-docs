56be4aa777ff66ec0d5b29361dc8de5071f74bfd
yousunzoo <dpfvmffjqm@gmail.com>
2025-03-26 20:11:19 +0900

    feat: review mock data 추가 및 타입 수정

--- src/entities/restaurant/model/types.ts
+++ src/entities/restaurant/model/types.ts
@@ -1,9 +1,18 @@
 export type Category = '한식' | '중식' | '일식' | '양식' | '아시안' | '기타';
 
+export type Review = {
+  id: string;
+  rating: number;
+  content: string;
+  author: string;
+  date: string;
+};
+
 export type Restaurant = {
   id: string;
   name: string;
   description: string;
   category: Category;
   isFavorite: boolean;
+  reviews: Review[];
 };
