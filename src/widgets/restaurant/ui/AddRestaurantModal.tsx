d84e7f102a5f08217840ab43a3f7568ecb9f371c
yousunzoo <dpfvmffjqm@gmail.com>
2025-03-29 19:30:11 +0900

    feat: AddRestaurantModal에서 useForm 훅을 사용하여 폼 관리 개선

--- src/widgets/restaurant/ui/AddRestaurantModal.tsx
+++ src/widgets/restaurant/ui/AddRestaurantModal.tsx
@@ -1,20 +1,12 @@
-import { useEffect, useState } from 'react';
-
 import { AddRestaurantData, CATEGORY_OPTIONS, addRestaurantData } from '@entities/restaurant';
 
-import { useMutation } from '@shared/hooks';
+import { useForm, useMutation } from '@shared/hooks';
 import { useFetchContext } from '@shared/providers';
 import { Drawer, Form } from '@shared/ui';
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
-};
-
-const checkedForm = (form: Partial<AddRestaurantData['request']>): form is AddRestaurantData['request'] => {
-  if (!form.name || !form.category || !form.description) return false;
-
-  return true;
 };
 
 const initialForm: Partial<AddRestaurantData['request']> = {
@@ -24,7 +16,6 @@ const initialForm: Partial<AddRestaurantData['request']> = {
 };
 
 export const AddRestaurantModal = ({ isOpen, onClose }: Props) => {
-  const [form, setForm] = useState<Partial<AddRestaurantData['request']>>(initialForm);
   const { triggerRefetch } = useFetchContext();
 
   const { mutate, isLoading } = useMutation({
@@ -38,33 +29,20 @@ export const AddRestaurantModal = ({ isOpen, onClose }: Props) => {
     },
   });
 
-  const isDisabled = !checkedForm(form);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (isDisabled) return;
-
-    mutate(form);
-  };
-
-  const handleChange =
-    (key: keyof typeof form) => (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-      setForm((prev) => ({ ...prev, [key]: e.target.value }));
-    };
-
-  useEffect(() => {
-    if (!isOpen) {
-      setForm(initialForm);
-    }
-  }, [isOpen]);
+  const formInstance = useForm<Omit<AddRestaurantData['request'], 'id'>>({
+    initialValues: initialForm,
+    onSubmit: (values) => {
+      mutate(values);
+    },
+    onReset: onClose,
+  });
 
   return (
     <Drawer isOpen={isOpen} onClose={onClose}>
       <h2 className="modal-title text-title">새로운 음식점</h2>
-      <Form name="add-restaurant-form" onSubmit={handleSubmit} onReset={onClose}>
+      <Form name="add-restaurant-form" form={formInstance}>
         <Form.Item name="category" label="카테고리" required>
-          <select name="category" id="category" required onChange={handleChange('category')}>
+          <select name="category" id="category" required>
             {CATEGORY_OPTIONS.map((option) => (
               <option key={option.value ?? 'default'} value={option.value}>
                 {option.label}
@@ -74,11 +52,11 @@ export const AddRestaurantModal = ({ isOpen, onClose }: Props) => {
         </Form.Item>
 
         <Form.Item name="name" label="이름" required>
-          <input type="text" name="name" id="name" required onChange={handleChange('name')} />
+          <input type="text" name="name" id="name" required />
         </Form.Item>
 
         <Form.Item name="description" label="설명">
-          <textarea name="description" id="description" cols={30} rows={5} onChange={handleChange('description')} />
+          <textarea name="description" id="description" cols={30} rows={5} />
 
           <span className="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
         </Form.Item>
@@ -86,7 +64,7 @@ export const AddRestaurantModal = ({ isOpen, onClose }: Props) => {
         <div className="button-container">
           <Form.Reset disabled={isLoading}>취소</Form.Reset>
 
-          <Form.Submit disabled={isDisabled || isLoading}>{isLoading ? '추가중...' : '추가하기'}</Form.Submit>
+          <Form.Submit disabled={formInstance.isError || isLoading}>{isLoading ? '추가중...' : '추가하기'}</Form.Submit>
         </div>
       </Form>
     </Drawer>
